784201c65acbfae4961d9940350043301a4f8117
Kiyotaka <t10kiyotaka@gmail.com>
2019-07-01 23:52:28 +0900

    Add validation

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -5,12 +5,23 @@ import play.api.data.Forms._
 
 object Person {
   val personForm: Form[PersonForm] = Form {
+    val regexForEmail = """[a-zA-Z0-9.+_-]+@[A-Z0-9.-]+\.[A-Z]"""
     mapping(
-      "name" -> text,
-      "mail" -> text,
-      "tel" -> text,
+      "name" -> nonEmptyText
+        .verifying("3文字以上で入力してください", s => isGreaterThanN(s, 2))
+        .verifying("10文字以下で入力してください", s => isLessThanN(s, 11)),
+      "mail" -> email
+        .verifying("正しいメールアドレスを入力してください", s => s.matches(regexForEmail)),
+      "tel" -> nonEmptyText
+        .verifying("10桁または11桁の半角数字で入力してください", s => s.matches("""[0-9]{10,11}""")),
     )(PersonForm.apply)(PersonForm.unapply)
   }
+
+  def isGreaterThanN(s: String, n: Int): Boolean =
+    if(s.length > n) true else false
+
+  def isLessThanN(s: String, n: Int): Boolean =
+    if(s.length < n) true else false
 
   val personFindForm: Form[PersonFind] = Form {
     mapping(
